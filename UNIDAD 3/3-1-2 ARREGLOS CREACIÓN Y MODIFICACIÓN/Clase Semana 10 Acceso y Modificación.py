7d1aa42f8352bd3e3882fb30052839116a72a8d7
SANTIAGO LARA <sa.larac@uea.edu.ec>
2025-02-22 14:31:10 -0500

    Semana 10

---
+++ UNIDAD 3/3-1-2 ARREGLOS CREACIÓN Y MODIFICACIÓN/Clase Semana 10 Acceso y Modificación.py
@@ -0,0 +1,15 @@
+matriz = [
+
+        [1,2,3],
+        [4,5,6]
+    ]
+
+print(matriz)
+matriz[1][2] = 666
+
+#Imprimir la matriz de manera estructurada
+print("\n Matriz Modificada")
+for fila in matriz:
+    print (fila)
+
+
